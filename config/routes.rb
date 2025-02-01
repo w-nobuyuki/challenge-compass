fc03083087d3167b79ccaa233242328f3a6ad5ab
nabesan <nabesan@sonicgarden.jp>
2025-02-01 20:11:25 +0900

    Googleのログイン機能を追加

--- config/routes.rb
+++ config/routes.rb
@@ -12,4 +12,6 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   root "posts#index"
+
+  devise_for :users, controllers: { omniauth_callbacks: "users/omniauth_callbacks" }
 end
